c1ff1478a6cb5ef860400264449cb758aac97823
Gabriel Vilela <gabrielvilelaportela@gmail.com>
2024-07-31 14:52:49 -0300

    Adicionar exercicios ate numero 30

---
+++ index.js
@@ -0,0 +1,179 @@
+/* Exercício 1 */
+
+console.log("    #    ");
+console.log("   ###   ");
+console.log("  #####  ");
+console.log(" ####### ");
+
+/* Exercício 2 */
+
+let character;
+
+/* Exercício 3 */
+
+let character = "Hello";
+
+/* Exercício 4 */
+
+let character = 'Hello';
+
+/* Exercício 5 */
+
+let character = 'Hello';
+console.log(character);
+
+/* Exercício 6 */
+
+let character = 'Hello';
+console.log(character);
+character = 'World'
+
+/* Exercício 7 */
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character)
+
+/* Exercício 8 */
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character)
+let secondCharacter;
+
+/* Exercício 9 */
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+console.log(secondCharacter);
+
+/* Exercício 10 */
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+console.log(secondCharacter);
+secondCharacter = "Test";
+
+/* Exercício 11 */
+
+secondCharacter = "Test";
+secondCharacter = character;
+
+/* Exercício 12 */
+
+let character = 'Hello';
+
+/* Exercício 13 */
+
+let profession = "teacher";
+let age;
+console.log(profession);
+console.log(age);
+
+/* Exercício 14 */
+
+let character = "Hello";
+
+/* Exercício 15 */
+
+let character = 'Hello';
+let count = 8;
+
+/* Exercício 16 */
+
+let character = 'Hello';
+let count = 8;
+console.log(count + 1)
+
+/* Exercício 17 */
+
+let count = 8;
+
+/* Exercício 18 */
+
+let character = 'Hello';
+let count = 8;
+let rows = [];
+
+/* Exercício 19 */
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+
+/* Exercício 20 */
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+
+/* Exercício 21 */
+
+rows[2] = 10
+console.log(rows)
+
+/* Exercício 22 */
+
+rows[2] = 10;
+rows[rows.length - 1]
+
+/* Exercício 23 */
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+
+/* Exercício 24 */
+
+let cities = ["London", "New York", "Mumbai"];
+console.log(cities);
+cities[2] = "Mexico City";
+console.log(cities);
+
+/* Exercício 25 */
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+
+/* Exercício 26 */
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
+
+/* Exercício 27 */
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let popped = rows.pop();
+console.log(popped);
+
+/* Exercício 28 */
+
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed);
+
+/* Exercício 29 */
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+
+/* Exercício 30 */
+
+let character = "Hello";
+let count = 8;
+let rows = [];
+
+
+
+
+
